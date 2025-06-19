d0a37e4ad2a3329f500cecf89f042b42690a7e28
Quan Gian <82435818+thq4n@users.noreply.github.com>
2025-06-19 14:34:13 +0700

    first commit

---
+++ bin/component_creator.dart
@@ -0,0 +1,5 @@
+import 'package:component_creator/component_creator.dart' as component_creator;
+
+void main(List<String> arguments) {
+  print('Hello world: ${component_creator.calculate()}!');
+}
---
+++ test/component_creator_test.dart
@@ -0,0 +1,8 @@
+import 'package:component_creator/component_creator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
